1d12bdb73460aba6f2facf3ca0627ef76b6fceef
SandroZakaidze <zakaidzesandro@gmail.com>
2023-11-09 10:31:51 +0000

    first commit

---
+++ src/components/Footer/Newsletter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
+
+
+import './Newsletter.css';
+
+const Newsletter = () => (
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="Newsletter"/> 
+      <h1></h1>
+
+    </div>
+   
+
+   
+  </div>
+);
+
+export default Newsletter;
